31f6ba834f7ceb4827b072a64946c6107bbb7848
mlo <martin.lott@mlo-dev.de>
2023-08-03 21:28:32 +0200

    TsImports can now have dependencies which are also automatically added

--- src/main/java/de/mlo/dev/tsbuilder/elements/imports/TsImport.java
+++ src/main/java/de/mlo/dev/tsbuilder/elements/imports/TsImport.java
@@ -14,6 +14,7 @@ import java.util.Set;
 public class TsImport {
     private final Set<String> moduleNames = new LinkedHashSet<>();
     private final String fromPath;
+    private final Set<TsImport> dependencies = new LinkedHashSet<>();
 
     public TsImport(String moduleName, String fromPath) {
         this.moduleNames.add(moduleName);
@@ -22,6 +23,11 @@ public class TsImport {
 
     public TsImport addModuleName(String moduleName){
         this.moduleNames.add(moduleName);
+        return this;
+    }
+
+    public TsImport addDependency(TsImport dependency){
+        this.dependencies.add(dependency);
         return this;
     }
 }
--- src/main/java/de/mlo/dev/tsbuilder/elements/imports/TsImportList.java
+++ src/main/java/de/mlo/dev/tsbuilder/elements/imports/TsImportList.java
@@ -9,11 +9,7 @@ import java.util.stream.Collectors;
 public class TsImportList extends LinkedHashSet<TsImport> {
 
     public String build(){
-        Map<String, Set<TsImport>> map = new LinkedHashMap<>();
-        for(TsImport tsImport : this){
-            Set<TsImport> tsImports = map.computeIfAbsent(tsImport.getFromPath(), path -> new LinkedHashSet<>());
-            tsImports.add(tsImport);
-        }
+        Map<String, Set<TsImport>> map = flattenAndDistinctImports();
         StringBuilder sb = new StringBuilder();
         for (Map.Entry<String, Set<TsImport>> entry : map.entrySet()) {
             String modules = entry.getValue()
@@ -25,5 +21,24 @@ public class TsImportList extends LinkedHashSet<TsImport> {
         }
         sb.append('\n');
         return sb.toString();
+    }
+
+    /**
+     * Walk through every added import and its dependencies and puts them into
+     * a map. The key of the map is the path ('from') to the module. One path
+     * can have multiple elements to import. Duplicated imports will be removed.
+     *
+     * @return A map with all declared imports
+     */
+    private Map<String, Set<TsImport>> flattenAndDistinctImports(){
+        Map<String, Set<TsImport>> map = new LinkedHashMap<>();
+        forEach(tsImport -> distinct(map, tsImport));
+        return map;
+    }
+
+    private void distinct(Map<String, Set<TsImport>> map, TsImport tsImport){
+        Set<TsImport> tsImports = map.computeIfAbsent(tsImport.getFromPath(), path -> new LinkedHashSet<>());
+        tsImports.add(tsImport);
+        tsImport.getDependencies().forEach(dependency -> distinct(map, dependency));
     }
 }
